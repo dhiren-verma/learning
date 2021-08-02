308865b954fc8a8a50223dda6ec2a49ffb368c84
Vermas-Dell <Vermas-Dell@Dell>
2021-08-02 18:40:21 +0530

    Refactored Class Name

--- spring-security-demo_bootstrap-login-form/src/main/java/com/learning/spring_security_demo/config/SpringSecurityWebApplicationInitializer.java
+++ spring-security-demo_bootstrap-login-form/src/main/java/com/learning/spring_security_demo/config/SpringSecurityWebApplicationInitializer.java
@@ -2,7 +2,7 @@ package com.learning.spring_security_demo.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer extends
-		AbstractSecurityWebApplicationInitializer {
-	
+public class SpringSecurityWebApplicationInitializer
+		extends AbstractSecurityWebApplicationInitializer {
+
 }
