3a242898e058f3b7bbc5a0ae7826978947eedfa4
Vermas-Dell <Vermas-Dell@Dell>
2021-09-20 01:32:17 +0530

    Basic Authentication with In-Memory Authentication

---
+++ spring-crm-rest-demo/src/main/java/com/learning/spring_crm_rest_demo/config/SpringSecuritWebAppInitializer.java
@@ -0,0 +1,8 @@
+package com.learning.spring_crm_rest_demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecuritWebAppInitializer
+		extends AbstractSecurityWebApplicationInitializer {
+	
+}
