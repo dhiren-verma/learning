06e7205ca7488f4e59ef305afc726b77c021cc7f
Vermas-Dell <Vermas-Dell@Dell>
2021-09-24 00:50:58 +0530

    Refactoring Updates

--- web-customer-tracker-all-java-config/src/main/java/com/learning/webCustomerTracker_AllJavaConfig/config/SpringSecurityConfiguration.java
+++ web-customer-tracker-all-java-config/src/main/java/com/learning/webCustomerTracker_AllJavaConfig/config/SpringSecurityConfiguration.java
@@ -1,23 +1,16 @@
 package com.learning.webCustomerTracker_AllJavaConfig.config;
 
-import java.util.logging.Logger;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -68,7 +61,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 	
 //	@Configuration
-//	@Order(2)
+//	@Order(1)
 //	public static class WebAppSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter{
 //		
 //		Logger logger = Logger.getLogger(getClass().getName());
@@ -103,7 +96,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 //	}
 //	
 //	@Configuration
-//	@Order(1)
+//	@Order(2)
 //	public static class RestApiSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 //		
 //		Logger logger = Logger.getLogger(getClass().getName());
