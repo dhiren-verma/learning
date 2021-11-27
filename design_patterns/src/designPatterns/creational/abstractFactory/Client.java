bd95b3a8aa059f3644bc45af35de020a65169ba6
dhiren-verma <dhirendraverma.ece@gmail.com>
2021-11-28 01:54:12 +0530

    Just updated Client as Test

--- design_patterns/src/designPatterns/creational/abstractFactory/Client.java
+++ design_patterns/src/designPatterns/creational/abstractFactory/Client.java
@@ -1,9 +1,23 @@
 package designPatterns.creational.abstractFactory;
 
+import designPatterns.creational.abstractFactory.computer.Computer;
+
 public class Client {
-	
+
 	public static void main(String[] args) {
+		Computer asusLaptop = ComputerFactory.getComputer(new AsusLaptopFactory());
+		Computer msiLaptop = ComputerFactory.getComputer(new MSILaptopFactory());
+		Computer cyberPowerDesktop = ComputerFactory.getComputer(new CyberPowerDesktopFactory());
+		Computer dellDesktop = ComputerFactory.getComputer(new DellDesktopFactory());
+		
+		System.out.println(asusLaptop);
+		System.out.println("\n\n");
+		System.out.println(msiLaptop);
+		System.out.println("\n\n");
+		System.out.println(cyberPowerDesktop);
+		System.out.println("\n\n");
+		System.out.println(dellDesktop);
 		
 	}
-	
+
 }
