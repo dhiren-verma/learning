7a63500347352f8c06cafb0e68f5b52004342f3e
dhiren-verma <dhirendraverma.ece@gmail.com>
2022-02-03 20:31:38 +0530

    fix : fix to enable tests
    Updated the name of test package to be the same as src packages.
    this was done so that the Tests can run, otherwise if the package name
    doesn't match between src and test, then tests fail to run

--- 02-spring-boot-devtools-demo/src/test/java/com/learning/spring_boot_devtools_demo/SpringBootDemoApplicationTests.java
+++ 02-spring-boot-devtools-demo/src/test/java/com/learning/spring_boot_devtools_demo/SpringBootDemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.learning.spring_boot_demo;
+package com.learning.spring_boot_devtools_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
