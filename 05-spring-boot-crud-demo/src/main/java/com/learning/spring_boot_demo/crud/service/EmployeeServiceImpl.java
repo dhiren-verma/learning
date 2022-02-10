f601758d1af55374d0df7f18a19452c7153fe519
dhiren-verma <dhirendraverma.ece@gmail.com>
2022-02-10 13:54:38 +0530

    add : added new spring boot crud using hibernate project
    Added Spring Boot CRUD using Hibernate with MySQL

---
+++ 05-spring-boot-crud-demo/src/main/java/com/learning/spring_boot_demo/crud/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.learning.spring_boot_demo.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.learning.spring_boot_demo.crud.dao.EmployeeDAO;
+import com.learning.spring_boot_demo.crud.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO empDao;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO empDao) {
+		this.empDao = empDao;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return empDao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int id) {
+		return empDao.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		empDao.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int id) {
+		empDao.deleteById(id);
+	}
+
+}
